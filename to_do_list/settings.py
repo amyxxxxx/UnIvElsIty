d1bda85930ab00fe5914eaad83fde70166e4930a
Favor <fayvz.xo@gmail.com>
2021-11-30 14:21:00 +0100

    minor tweaks

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['to-do-list-api-app.herokuapp.com']
 
 
 # Application definition
